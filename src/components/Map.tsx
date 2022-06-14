914279d9b6531b1a61df52cab9a37761913131a3
Fuqiao Xue <xfq@w3.org>
2022-06-14 10:16:12 +0800

    Tweak the map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -18,12 +18,20 @@ const Map: React.FC<MapProps> = ({ mapCenter, locations }) => {
       plugins: [], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
     })
       .then((AMap) => {
-        var map = new AMap.Map("map-container", {
-          zoom: 16, //级别
+        var marker, map = new AMap.Map("map-container", {
+          resizeEnable: true,
+          zoom: 15, //级别
           // center: [mapCenter.lat, mapCenter.lng], //中心点坐标
-          center: [116.353792, 39.98766], //中心点坐标
+          center: [116.35178167430497, 39.980318073213354], //中心点坐标
           // center: [116.397428, 39.90923] //初始化地图中心点
         });
+
+        marker = new AMap.Marker({
+          icon: "https://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
+          position: [116.35178167430497, 39.980318073213354]
+      });
+      marker.setMap(map);
+
       })
       .catch((e) => {
         console.log(e);
